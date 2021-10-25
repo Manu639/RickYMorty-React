bed864dbe48956e107e5209ab1207a2e999ea592
Manuel Castillo <m.castillo@gestionescastillo.com>
2021-10-25 17:20:04 +0200

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [personajes, setPersonajes] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/character')
+      .then((response) => setPersonajes(response.data.results))
+      .catch((err) => console.log(err))
+  }, []);
+
+
+  console.log(personajes)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {personajes.map(personaje =>
+        <div class="card">
+          <img src={personaje.image} alt={personaje.name} />
+          <div class="container">
+            <h4><b>{personaje.name}</b></h4>
+            <p>{personaje.status}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+/*
+react-router
+Formularios: formik, react hook form
+Redux
+Hooks: useContext, useReducer...
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
